0c2f81a9350dad48b4cf7378e21122763da4ca13
Patriciabadez <patriciabaldez1985@gmail.com>
2021-10-21 18:40:58 -0300

    conclusao da requisicao das apis nas paginas

--- pokedex/src/Pages/DetailsPage/DetailsPage.js
+++ pokedex/src/Pages/DetailsPage/DetailsPage.js
@@ -4,9 +4,24 @@ import { useHistory, useParams } from "react-router";
 import Header from "../../components/Header";
 import { ConteinerDetalhes, DivImagens, DivStatus, ConteinerTipoAtaques, DivTipos, DivAtaques } from "./Styled"
 
+
 export const DetailsPage = () => {
-    const [imagemFrente, setImagemFrente] = useState({})
-    const [imagemCostas, setImagemCostas] = useState({})
+    const [imagemFrente, setImagemFrente] = useState()
+    const [imagemCostas, setImagemCostas] = useState()
+    const [vidaHp,setVidaHp] = useState()
+    const [attack, setAttack] = useState()
+    const [defense, setDefense] = useState()
+    const [specialDefense, setEspecialDefense] = useState()
+    const [specialAttack, setEspecialAttack] = useState()
+    const [speed, setSpeed] = useState()
+    const [type1, setType1] = useState()
+    const [type2, setType2] = useState()
+    const [ataque1, setAtaque1]=useState()
+    const [ataque2, setAtaque2]=useState()
+    const [ataque3, setAtaque3]=useState()
+    const [ataque4, setAtaque4]=useState()
+    const [ataque5, setAtaque5]=useState()
+    
 
     const params = useParams()
     console.log( "seu sou params", params)
@@ -15,20 +30,34 @@ export const DetailsPage = () => {
     const irParaHome = () => {
         history.push("/")
     }
+
     useEffect(()=>{
         axios.get(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
         .then((res) => {
             setImagemFrente( res.data.sprites.versions['generation-v']['black-white'].animated.front_default)
             setImagemCostas(res.data.sprites.versions['generation-v']['black-white'].animated.back_default)
+            setVidaHp(res.data.stats[0].base_stat)
+            setAttack (res.data.stats[1].base_stat)
+            setDefense (res.data.stats[2].base_stat)
+            setEspecialAttack (res.data.stats[3].base_stat)
+            setSpeed (res.data.stats[5].base_stat)
+            setEspecialDefense (res.data.stats[4].base_stat)
+            setType1(res.data.types[0].type.name)
+            console.log(res.data.types[0].type.name)
+            setAtaque1(res.data.moves[0].move.name)
+            console.log(res.data)
+            setAtaque2(res.data.moves[1].move.name)
+            setAtaque3(res.data.moves[2].move.name)
+            setAtaque4(res.data.moves[3].move.name)
+            setAtaque5(res.data.moves[4].move.name)
+            setType2(res.data.types[1].type.name)
+            
         }).catch((err)=>{
+         
             console.log(err.response)
         })
     },[])
     
- 
-
-   
-
     return (
         <div>
             <Header mudarPagina = {irParaHome} nome = {"Ir Para Home"}/>
@@ -39,25 +68,25 @@ export const DetailsPage = () => {
                 </DivImagens>
                 <DivStatus>
                     <h2>Status</h2>
-                    <p><b>hp:</b> 45</p>
-                    <p><b>attack:</b>23</p>
-                    <p><b>defense: </b>54</p>
-                    <p><b>defense: </b>35</p>
-                    <p><b>special-defense:</b>26</p>
-                    <p><b>speed: </b>87</p>
+                    <p><b>hp:</b> {vidaHp}</p>
+                    <p><b>attack:</b>{attack}</p>
+                    <p><b>defense: </b>{defense}</p>
+                    <p><b>special-attack: </b>{specialAttack}</p>
+                    <p><b>special-defense:</b>{specialDefense}</p>
+                    <p><b>speed: </b>{speed}</p>
                 </DivStatus>
                 <ConteinerTipoAtaques>
                     <DivTipos>
-                        <p><b>tipo1</b></p>
-                        <p><b>tipo2</b></p>
+                        <p><b>{type1}</b></p>
+                        <p><b>{type2}</b></p>
                     </DivTipos>
                     <DivAtaques>
                         <h2>Principais Ataques</h2>
-                        <p>Ataque1</p>
-                        <p>Ataque2</p>
-                        <p>Ataque3</p>
-                        <p>Ataque4</p>
-                        <p>Ataque5</p>
+                        <p>{ataque1}</p>
+                        <p>{ataque2}</p>
+                        <p>{ataque3}</p>
+                        <p>{ataque4}</p>
+                        <p>{ataque5}</p>
                     </DivAtaques>
                 </ConteinerTipoAtaques>
             </ConteinerDetalhes>
